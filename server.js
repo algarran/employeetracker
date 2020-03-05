b3e50e5b623854b79fbcfc2bc09c208b7eacf4fa
Nick Algarra <algarran24@gmail.com>
2020-03-04 20:27:02 -0600

    figlet function

--- server.js
+++ server.js
@@ -2,3 +2,24 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const figlet = require("figlet");
 
+async function fancyFigs(){
+    console.log(figlet.textSync("Awesome", {
+        font: "standard",
+        horizontalLayout: "default",
+        verticalLayout: "default"
+    }))
+    console.log(figlet.textSync("Employee", {
+        font: "standard",
+        horizontalLayout: "default",
+        verticalLayout: "default"
+    }))
+    console.log(figlet.textSync("Tracker", {
+        font: "standard",
+        horizontalLayout: "default",
+        verticalLayout: "default"
+    }))
+};
+
+fancyFigs();
+
+
