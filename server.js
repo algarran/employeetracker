78d2792b4e66ef5b377baba5879fa32a967c6139
Nick Algarra <algarran24@gmail.com>
2020-03-04 20:31:04 -0600

    inserted beginning of user prompts

--- server.js
+++ server.js
@@ -34,3 +34,16 @@ connection.connect(function(err){
     if (err) throw (err)
 });
 
+function start(){
+    inquirer.prompt([{
+        type: "list",
+        name: "options",
+        message: "What would you like to do?",
+        choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager",
+        "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
+    }])
+};
+
+start();
+
+
