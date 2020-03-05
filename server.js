95dea598f54c0155b1002df47d88ab6923c186f4
Nick Algarra <algarran24@gmail.com>
2020-03-04 20:29:32 -0600

    added mysql connection

--- server.js
+++ server.js
@@ -22,4 +22,15 @@ async function fancyFigs(){
 
 fancyFigs();
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Password",
+    database: "company_db"
+});
+
+connection.connect(function(err){
+    if (err) throw (err)
+});
 
